3d9815f6b506ba5a8436d4e47c4da49b120715a5
Szymon Miks <miks.szymon@gmail.com>
2021-01-18 13:22:03 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# surebets-finder
+
+Try to find as much as possible sure bets on the bookmaker websites
---
+++ tests/test_surebets_finder.py
@@ -0,0 +1,5 @@
+from surebets_finder import __version__
+
+
+def test_version():
+    assert __version__ == '0.1.0'
